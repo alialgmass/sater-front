2efe7dd0ae0fa40cff9d85acef717339df09ab22
ali <eali7563@gmail.com>
2026-02-15 22:19:17 +0200

    feat: add Navigation component with dynamic category links.

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,60 +1,7 @@
 <template>
     <ul>
         <li>
-            <n-link :to="localePath('/')">{{ $t('home') }} <i class="fa fa-angle-down"></i></n-link>
-            <ul class="mega-menu mega-menu-padding">
-                <li>
-                    <ul>
-                        <li class="mega-menu-title">
-                            <n-link to="">Home Group 01</n-link>
-                        </li>
-                        <li>
-                            <n-link to="/">Home Fashion One</n-link>
-                        </li>
-                        <li>
-                            <n-link to="/home-fashion-two">Home Fashion Two</n-link>
-                        </li>
-                        <li>
-                            <n-link to="/home-fashion-three">Home Fashion Three</n-link>
-                        </li>
-                        <li>
-                            <n-link to="/home-book-store">Home Book Store</n-link>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <ul>
-                        <li class="mega-menu-title">
-                            <n-link to="">Home Group 02</n-link>
-                        </li>
-                        <li>
-                            <n-link to="/home-fashion-four">Home Fashion Four</n-link>
-                        </li>
-                        <li>
-                            <n-link to="/home-fashion-five">Home Fashion Five</n-link>
-                        </li>
-                        <li>
-                            <n-link to="/home-fashion-six">Home Fashion Six</n-link>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <ul>
-                        <li class="mega-menu-title">
-                            <n-link to="">Home Group 03</n-link>
-                        </li>
-                        <li>
-                            <n-link to="/home-fashion-seven">Home Fashion Seven</n-link>
-                        </li>
-                        <li>
-                            <n-link to="/home-cosmetics">Home Cosmetics</n-link>
-                        </li>
-                        <li>
-                            <n-link to="/home-furniture">Home Furniture</n-link>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
+            <n-link :to="localePath('/')">{{ $t('home') }}</n-link>
         </li>
         <li>
             <n-link :to="localePath('/shop')"> {{ $t('shop') }} <i class="fa fa-angle-down"></i></n-link>
@@ -72,39 +19,10 @@
             </ul>
         </li>
         <li>
-            <n-link to="">{{ $t('pages') }} <i class="fa fa-angle-down"></i></n-link>
-            <ul class="submenu">
-                <li><n-link :to="localePath('/cart')">{{ $t('cart') }}</n-link></li>
-                <li><n-link :to="localePath('/checkout')">{{ $t('checkout') }}</n-link></li>
-                <li><n-link :to="localePath('/wishlist')">{{ $t('wishlist') }}</n-link></li>
-                <li><n-link :to="localePath('/compare')">{{ $t('compare') }}</n-link></li>
-                <li><n-link :to="localePath('/about')">{{ $t('about_us') }}</n-link></li>
-                <li><n-link :to="localePath('/my-account')">{{ $t('my_account') }}</n-link></li>
-                <li><n-link :to="localePath('/login-register')">{{ $t('login') }} / {{ $t('register') }}</n-link></li>
-                <li><n-link :to="localePath('/contact')">{{ $t('contact') }}</n-link></li>
-                <li><n-link :to="localePath('/privacy-policy')">Privacy Policy</n-link></li>
-                <li><n-link :to="localePath('/terms-conditions')">Terms Conditions</n-link></li>
-            </ul>
+            <n-link :to="localePath('/about')">{{ $t('about_us') }}</n-link>
         </li>
         <li>
-            <n-link :to="localePath('/blog')">{{ $t('blog') }} <i class="fa fa-angle-down"></i></n-link>
-            <ul class="submenu">
-                <li>
-                    <n-link to="/blog">blog standard</n-link>
-                </li>
-                <li>
-                    <n-link to="/blog/blog-right-sidebar">blog right sidebar</n-link>
-                </li>
-                <li>
-                    <n-link to="/blog/blog-without-sidebar">blog No sidebar</n-link>
-                </li>
-                <li>
-                    <n-link to="/blog/a-guide-to-latest-trends-product">blog details</n-link>
-                </li>
-            </ul>
-        </li>
-        <li>
-            <n-link :to="localePath('/contact')">{{ $t('contact') }} </n-link>
+            <n-link :to="localePath('/contact')">{{ $t('contact') }}</n-link>
         </li>
     </ul>
 </template>
