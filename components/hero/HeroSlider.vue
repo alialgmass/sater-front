50a67a0cb0c48f37a3b6eacd161f79912d4b37fd
ali <eali7563@gmail.com>
2026-02-16 01:19:44 +0200

    feat: introduce HeroSlider component to display dynamic banners with Swiper.

--- components/hero/HeroSlider.vue
+++ components/hero/HeroSlider.vue
@@ -1,36 +1,38 @@
 <template>
     <div class="slider-area nav-style-1">
-        <swiper :options="swiperOption" v-if="banners.length > 0">
-            <swiper-slide v-for="(banner, index) in banners" :key="index" class="single-slider slider-height-1 bg-purple">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="slider-content slider-animation-1">
-                                <h3>{{ banner.title }}</h3>
-                                <h1 v-html="banner.description"></h1>
-                                <div class="slider-btn btn-hover">
-                                    <n-link :to="localePath('/shop')">{{ $t('shop_now') }}</n-link>
+        <client-only>
+            <swiper :options="swiperOption" v-if="banners.length > 0">
+                <swiper-slide v-for="(banner, index) in banners" :key="index" class="single-slider slider-height-1 bg-purple">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="slider-content slider-animation-1">
+                                    <h3>{{ banner.title }}</h3>
+                                    <h1 v-html="banner.description"></h1>
+                                    <div class="slider-btn btn-hover">
+                                        <n-link :to="localePath('/shop')">{{ $t('shop_now') }}</n-link>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="slider-single-img slider-img-animation">
+                                    <img :src="banner.image_url" :alt="banner.title">
                                 </div>
                             </div>
                         </div>
-                        <div class="col-sm-6">
-                            <div class="slider-single-img slider-img-animation">
-                                <img :src="banner.image_url" :alt="banner.title">
-                            </div>
-                        </div>
                     </div>
-                </div>
-            </swiper-slide>
+                </swiper-slide>
 
-            <!-- Swiper Navigation Start -->
-            <div class="hero-slider-nav swiper-button-prev">
-                <i class="pe-7s-angle-left"></i>
-            </div>
-            <div class="hero-slider-nav swiper-button-next">
-                <i class="pe-7s-angle-right"></i>
-            </div>
-            <!-- Swiper Navigation End -->
-        </swiper>
+                <!-- Swiper Navigation Start -->
+                <div class="hero-slider-nav swiper-button-prev">
+                    <i class="pe-7s-angle-left"></i>
+                </div>
+                <div class="hero-slider-nav swiper-button-next">
+                    <i class="pe-7s-angle-right"></i>
+                </div>
+                <!-- Swiper Navigation End -->
+            </swiper>
+        </client-only>
     </div>
 </template>
 
